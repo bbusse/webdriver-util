7b29ec6ec5b8a779c72c6a05c60532f23580caee
Björn Busse <bj.rn@baerlin.eu>
2021-02-05 14:00:12 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+from setuptools import find_packages, setup
+
+setup(name='webdriver-util',
+      version='0.2.0',
+      description='webdriver util',
+      url='https://github.com/bbusse/webdriver-util',
+      author='Björn Busse',
+      author_email='bj.rn@baerlin.eu',
+      license='BSD 3-Clause License',
+      packages=find_packages('.', exclude=['tests']),
+      install_requires=["configparse  >= 1.2.3",
+                        "selenium >= 3.141.0"],
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3.9",
+      ],
+      zip_safe=False)
